46f0438d58865a37afbb6b005a6938ceb8026839
Marvin Block <mblock@kodi.de>
2020-05-06 10:42:19 +0000

    Fixed getting user and adjusted displayAvatarURL

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,29 +1,29 @@
 const Discord = require("discord.js");
 const logger = require("../modules/logger.js")
 
-module.exports.run = async(client, message, args) => {
+module.exports.run = async (client, message, args) => {
     try {
         if (args[0] == undefined) {
-            client.fetchUser(message.author.id).then(function(value) {
-                const embed = new Discord.RichEmbed()
-                    .setColor(53380)
-                    .setTitle("Hier der Avatar von " + value.username)
-                    .setImage(value.displayAvatarURL)
-                message.channel.send({
-                    embed: embed
-                })
+            let user = client.users.cache.get(message.author.id)
+            const embed = new Discord.MessageEmbed()
+                .setColor(53380)
+                .setTitle("Hier der Avatar von " + user.username)
+                .setImage(user.displayAvatarURL({dynamic:true,size:4096}))
+            message.channel.send({
+                embed: embed
             })
+
         } else {
             const mentioned = args[0].replace(/^\D+|\D+$/g, "")
-            client.fetchUser(mentioned).then(function(value) {
-                const embed = new Discord.RichEmbed()
-                    .setColor(53380)
-                    .setTitle("Hier der Avatar von " + value.username)
-                    .setImage(value.displayAvatarURL)
-                message.channel.send({
-                    embed: embed
-                })
+            let user = client.users.cache.get(mentioned)
+            const embed = new Discord.MessageEmbed()
+                .setColor(53380)
+                .setTitle("Hier der Avatar von " + user.username)
+                .setImage(user.displayAvatarURL({dynamic:true,size:4096}))
+            message.channel.send({
+                embed: embed
             })
+
         }
     } catch (error) {
         logger.run("error", error)
