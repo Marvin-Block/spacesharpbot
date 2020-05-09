4ea7daf15ee9e9aaaf1ad79c0dad1601266e2649
unknown <marvin.block1998@gmail.com>
2020-05-09 20:47:46 +0200

    Added module for activity rating (WIP)

---
+++ modules/activity.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const logger = require("../modules/logger.js")
+
+module.exports.run = async(client, oldchannel, newchannel) => {
+    try {
+        //WORK IN PROGRESS
+    } catch (error) {
+        logger.run("error", error)
+    }
+}
